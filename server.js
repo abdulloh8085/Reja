fc197bd36b99b5ccd67eb27c995a30bc589726e8
abdulloh8085 <abdulloh.im4@gmail.com>
2026-04-21 23:42:19 +0900

    BRR: build express Web server

---
+++ server.js
@@ -0,0 +1,29 @@
+console.log('web server ishga tushdi');
+const express = require("express");
+const app = express();
+const http = require ("http");
+
+//1 Kirish codelar yoziladi
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2; Session codelar 
+
+//3: views codelar yozamiz
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+//4 Routing code lar yoziladi
+app.get("/hello", function(req, res) {
+    res.end("<h1>Hello World by Lincoln</h1>");
+});
+app.get("/Gift", function(req, res) {
+    res.end("<h1> Siz sovgalar saxifasidasiz!</h1>");
+});
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+    console.log(`the server is running successfully on port: ${PORT}`);
+});
---
+++ train.js
@@ -0,0 +1,8 @@
+// B-task
+
+//A-task
+
+// console.log('train task ishga tushdii!')
+console.log("Train Area is running now")
+
+// d---task
