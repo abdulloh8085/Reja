b95da01e0f741a61fd8ab313a12c1a6defe11c54
abdulloh8085 <abdulloh.im4@gmail.com>
2026-04-25 17:35:22 +0900

    BRR : completed MITask *A*

--- train.js
+++ train.js
@@ -1,8 +1,24 @@
-// B-task
+const express = require("express");
+const app = express();
+// =========================================== //
+// MiTask *A*
+// ========================================== //
+function berilganharflarnisanash(harf, soz) {
+    let count = 0;
+    for (let i = 0; i < soz.length; i++) {
+        if (soz[i] === harf) {
+            count++;
+        }
+    }
+    return count;
+}
 
-//A-task
+app.get("/", (req, res) => {
+    const result = berilganharflarnisanash("a", "Software engineer");
+    res.send(`Natija: ${result}`);
+});
 
-// console.log('train task ishga tushdii!')
-console.log("Train Area is running now")
 
-// d---task
+app.listen(3000, () => {
+console.log("Natijani shu saytdan ko'rishingiz mumkun!!! http://localhost:3000");
+});
