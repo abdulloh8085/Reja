8674cf689688879b24465f6a53014059483edef5
abdulloh8085 <abdulloh.im4@gmail.com>
2026-04-25 19:34:32 +0900

    feat: train callback hamda asyn functions

--- train2.js
+++ train2.js
@@ -5,9 +5,10 @@ const list = [
     "o'z ustingizda ishlashni boshlang ", // 30-40
     "siz kuchli bo'lgan narsalarni qiling", //40-50
     "Yoshlarga invest qiling", // 50-60
-    "endi dam oling, yetib kelib qoldi baribir)))", //60+
+    "endi dam oling, oz qoldi buyog'i!", //60
 ];
 
+// Callback function
 function maslahatBering(a, callback) {
     if (typeof a !== "number") callback("insert a number", null);
     else if (a <= 20) callback(null, list[0]); 
@@ -24,3 +25,46 @@ maslahatBering(10,( err, data) => {
     if (err) console.log("ERROR:", err); 
     console.log("javob:", data);
 });
+// async function
+// async function maslahatBering(a) {
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return list[0];  
+//     else if (a > 20 && a <= 30) return list[1]; 
+//     else if (a > 30 && a <= 40) return list[2]; 
+//     else if (a > 40 && a <= 50) return list[3]; 
+//     else if (a > 50 && a <= 60) return list[4]; 
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 4000);
+//         }); 
+//     }
+// }
+        // setTimeout(function () {
+        // return lsit[5]; 
+        // }, 4400);
+    
+
+// call via then/catch 
+// console.log("passed here 0");
+// maslahatBering(66)
+// .then((data) => { 
+    // console.log ("javob:", data); 
+    // })
+    // .catch((err) => {
+        // console.log("ERROR";, err)
+    // });
+    // console.log("passed here 1");
+
+// call via async/await
+// async function  run () {
+// let javob = await maslahatBering(20);
+// console.log(javob);
+// javob = await maslahatBering (33);
+// console.log(javob);
+// javob = await maslahatBering(44);
+// console.log(javob);
+// }
+
+// run();
