43f03d5bdf1de30d0eaffd90446f69727a7aa579
abdulloh8085 <abdulloh.im4@gmail.com>
2026-04-30 18:21:38 +0900

    feat:completed portfolio author page

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ console.log('web server ishga tushdi');
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
+const http = require("http");
 
 const fs = require("fs");
 
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//     if(err) { 
-//         console.log("ERROR:",  err);
-//     } else {
-//         user = JSON.parse(data)
-//     }
-// });
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) { 
+        console.log("ERROR:",  err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // Mongo DB connect 
 const db = require("./server").db();
 
 
 //1 Kirish codelar yoziladi
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -39,8 +40,9 @@ app.set("view engine", "ejs");
 // });
 
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
+    console.log("user entered /create-item");
     const new_reja = req.body.reja;
+
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
         if (err) {
     console.log(err);
@@ -52,9 +54,10 @@ app.post("/create-item", (req, res) => {
     });
 });
 
-// app.get('/author', (req, res) => {
-//     res.render("author", { user: user});
-// });
+app.get('/author', (req, res) => {
+    res.render("author", { user: user});
+}); 
+
 // app.get("/", function (req, res) {
 //     res.render("reja");
 // });
--- train.js
+++ train.js
@@ -17,98 +17,98 @@ function getNumberCount(text) {
   return digitsCount;
 }
 
-console.log(getNumberCount("Abdulloh ID 2026043019"));
+console.log(getNumberCount("Abdulloh ID: 20051008"));
 
 // =========================================== //
 // MiTask *A*
 // ========================================== //
-function berilganharflarnisanash(harf, soz) {
-    let count = 0;
-    for (let i = 0; i < soz.length; i++) {
-        if (soz[i] === harf) {
-            count++;
-        }
-    }
-    return count;
-}
+// function berilganharflarnisanash(harf, soz) {
+//     let count = 0;
+//     for (let i = 0; i < soz.length; i++) {
+//         if (soz[i] === harf) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
-app.get("/", (req, res) => {
-    const result = berilganharflarnisanash("o", "Salom mening ismim Odiljon, Yoshim 45da, Koreya yeongcheon shaxrida istiqomat qilaman, O'zbekman.");
-    res.send(`Natija: ${result}`);
-});
+// app.get("/", (req, res) => {
+//     const result = berilganharflarnisanash("o", "Salom mening ismim Odiljon, Yoshim 45da, Koreya yeongcheon shaxrida istiqomat qilaman, O'zbekman.");
+//     res.send(`Natija: ${result}`);
+// });
 
 
-app.listen(3000, () => {
-console.log("Natijani shu saytdan ko'rishingiz mumkun!!! http://localhost:3000");
-});
-// ================================================================================================================================
-console.log("Jack Ma Maslaxatlari");
-const list = [
-    "yaxshi talaba bo'ling", //0-20
-    "to'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
-    "o'z ustingizda ishlashni boshlang ", // 30-40
-    "siz kuchli bo'lgan narsalarni qiling", //40-50
-    "Yoshlarga invest qiling", // 50-60
-    "endi dam oling, oz qoldi buyog'i!", //60
-];
+// app.listen(3000, () => {
+// console.log("Natijani shu saytdan ko'rishingiz mumkun!!! http://localhost:3000");
+// });
+// // ================================================================================================================================
+// console.log("Jack Ma Maslaxatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", //0-20
+//     "to'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
+//     "o'z ustingizda ishlashni boshlang ", // 30-40
+//     "siz kuchli bo'lgan narsalarni qiling", //40-50
+//     "Yoshlarga invest qiling", // 50-60
+//     "endi dam oling, oz qoldi buyog'i!", //60
+// ];
 
-// Callback function
-function maslahatBering(a, callback) {
-    if (typeof a !== "number") callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]); 
-    else if (a > 20 && a <= 30) callback(null, list[1]); 
-    else if (a > 30 && a <= 40) callback(null, list[2]); 
-    else if (a > 40 && a <= 50) callback(null, list[3]); 
-    else if (a > 50 && a <= 60) callback(null, list[4]); 
-    else {
-        callback(null, list[5]);
-    }
-}
- 
-maslahatBering(10,( err, data) => { 
-    if (err) console.log("ERROR:", err); 
-    console.log("javob:", data);
-});
-// async function
-// async function maslahatBering(a) {
-//     if (typeof a !== "number") throw new Error("insert a number");
-//     else if (a <= 20) return list[0];  
-//     else if (a > 20 && a <= 30) return list[1]; 
-//     else if (a > 30 && a <= 40) return list[2]; 
-//     else if (a > 40 && a <= 50) return list[3]; 
-//     else if (a > 50 && a <= 60) return list[4]; 
+// // Callback function
+// function maslahatBering(a, callback) {
+//     if (typeof a !== "number") callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]); 
+//     else if (a > 20 && a <= 30) callback(null, list[1]); 
+//     else if (a > 30 && a <= 40) callback(null, list[2]); 
+//     else if (a > 40 && a <= 50) callback(null, list[3]); 
+//     else if (a > 50 && a <= 60) callback(null, list[4]); 
 //     else {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve(list[5]);
-//             }, 4000);
-//         }); 
+//         callback(null, list[5]);
 //     }
 // }
-        // setTimeout(function () {
-        // return lsit[5]; 
-        // }, 4400);
+ 
+// maslahatBering(10,( err, data) => { 
+//     if (err) console.log("ERROR:", err); 
+//     console.log("javob:", data);
+// });
+// // async function
+// // async function maslahatBering(a) {
+// //     if (typeof a !== "number") throw new Error("insert a number");
+// //     else if (a <= 20) return list[0];  
+// //     else if (a > 20 && a <= 30) return list[1]; 
+// //     else if (a > 30 && a <= 40) return list[2]; 
+// //     else if (a > 40 && a <= 50) return list[3]; 
+// //     else if (a > 50 && a <= 60) return list[4]; 
+// //     else {
+// //         return new Promise((resolve, reject) => {
+// //             setTimeout(() => {
+// //                 resolve(list[5]);
+// //             }, 4000);
+// //         }); 
+// //     }
+// // }
+//         // setTimeout(function () {
+//         // return lsit[5]; 
+//         // }, 4400);
     
 
-// call via then/catch 
-// console.log("passed here 0");
-// maslahatBering(66)
-// .then((data) => { 
-    // console.log ("javob:", data); 
-    // })
-    // .catch((err) => {
-        // console.log("ERROR";, err)
-    // });
-    // console.log("passed here 1");
+// // call via then/catch 
+// // console.log("passed here 0");
+// // maslahatBering(66)
+// // .then((data) => { 
+//     // console.log ("javob:", data); 
+//     // })
+//     // .catch((err) => {
+//         // console.log("ERROR";, err)
+//     // });
+//     // console.log("passed here 1");
 
-// call via async/await
-// async function  run () {
-// let javob = await maslahatBering(20);
-// console.log(javob);
-// javob = await maslahatBering (33);
-// console.log(javob);
-// javob = await maslahatBering(44);
-// console.log(javob);
-// }
+// // call via async/await
+// // async function  run () {
+// // let javob = await maslahatBering(20);
+// // console.log(javob);
+// // javob = await maslahatBering (33);
+// // console.log(javob);
+// // javob = await maslahatBering(44);
+// // console.log(javob);
+// // }
 
-// run();
+// // run();
